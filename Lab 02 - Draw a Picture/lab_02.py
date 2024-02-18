c8d5b3d98f0ced1356d0c627f56918c6cf9e22bd
FazliddinFayziev <fazrez4515@gmail.com>
2024-02-18 14:53:46 -0500

    Moving functions to another file

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -1,73 +1,10 @@
 import arcade
+from .functions import draw_two, draw_four
 
 # Constants
 SCREEN_TITLE = "Python Image"
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 500
-    
-
-def draw_two(r_w, r_h, value, top, color_1, color_2):
-    white = arcade.color.WHITE
-    
-    if top == True: 
-        move = r_h*value+10
-        
-        # BLOCK ONE
-        arcade.draw_rectangle_filled((SCREEN_WIDTH/2) - (r_w/2), move, r_w, r_h, color_1)
-        # arcade.draw_rectangle_outline((SCREEN_WIDTH/2) - (r_w/2), move, r_w, r_h, white)
-
-        # BLOCK TWO
-        arcade.draw_rectangle_filled((SCREEN_WIDTH/2) + (r_w/2), r_h*value, r_w, r_h+20, color_2)
-        # arcade.draw_rectangle_outline((SCREEN_WIDTH/2) + (r_w/2), r_h*value, r_w, r_h+20, white)
-        
-    else:
-        move = r_h*value-10
-        
-        # BLOCK ONE
-        arcade.draw_rectangle_filled((SCREEN_WIDTH/2) - (r_w/2), r_h*value, r_w, r_h+20, color_1)
-        # arcade.draw_rectangle_outline((SCREEN_WIDTH/2) - (r_w/2), r_h*value, r_w, r_h+20, white)
-    
-        # BLOCK TWO
-        arcade.draw_rectangle_filled((SCREEN_WIDTH/2) + (r_w/2), move, r_w, r_h, color_2)
-        # arcade.draw_rectangle_outline((SCREEN_WIDTH/2) + (r_w/2), move, r_w, r_h, white)
-    
-    
-def draw_four(r_w, r_h, value, top, color_1, color_2, color_3, color_4):
-    # arcade.draw_rectangle_filled(left, bottom, width, height, color)
-    white = arcade.color.WHITE
-    
-    # BLOCK ONE
-    if top:
-        arcade.draw_rectangle_filled((((SCREEN_WIDTH/2) - r_w*2) + r_w/2)-5, (r_h*value)-8, r_w, r_h+16, color_1)
-        # arcade.draw_rectangle_outline((((SCREEN_WIDTH/2) - r_w*2) + r_w/2)-5, (r_h*value)-8, r_w, r_h+17, white)
-    else:
-        arcade.draw_rectangle_filled((((SCREEN_WIDTH/2) - r_w*2) + r_w/2)-5, r_h*value, r_w, r_h, color_1)
-        # arcade.draw_rectangle_outline((((SCREEN_WIDTH/2) - r_w*2) + r_w/2)-5, r_h*value, r_w, r_h, white)
-    
-    # BLOCK TWO
-    if top:  
-        arcade.draw_rectangle_filled((SCREEN_WIDTH/2) + (r_w/2), r_h*value, r_w, r_h, color_2)
-        # arcade.draw_rectangle_outline((SCREEN_WIDTH/2) + (r_w/2), r_h*value, r_w, r_h, white)
-    else:
-        arcade.draw_rectangle_filled((SCREEN_WIDTH/2) + (r_w/2), r_h*value, r_w+10, r_h, color_2)
-        # arcade.draw_rectangle_outline((SCREEN_WIDTH/2) + (r_w/2), r_h*value, r_w+10, r_h, white)
-    
-    # BLOCK THREE
-    if top == False: 
-        arcade.draw_rectangle_filled((SCREEN_WIDTH/2) - (r_w/2), r_h*value, r_w, r_h, color_3)
-        # arcade.draw_rectangle_outline((SCREEN_WIDTH/2) - (r_w/2), r_h*value, r_w, r_h, white)
-    else:
-        arcade.draw_rectangle_filled((SCREEN_WIDTH/2) - (r_w/2), r_h*value, r_w+10, r_h, color_3)
-        # arcade.draw_rectangle_outline((SCREEN_WIDTH/2) - (r_w/2), r_h*value, r_w+10, r_h, white)
-        
-    
-    # BLOCK FOUR
-    if top == False:
-        arcade.draw_rectangle_filled((((SCREEN_WIDTH/2) + r_w*2) - r_w/2)+5, r_h*value+8, r_w, r_h+16, color_4)
-        # arcade.draw_rectangle_outline((((SCREEN_WIDTH/2) + r_w*2) - r_w/2)+5, r_h*value+9, r_w, r_h+17, white)
-    else:
-        arcade.draw_rectangle_filled((((SCREEN_WIDTH/2) + r_w*2) - r_w/2)+5, r_h*value, r_w, r_h, color_4)
-        # arcade.draw_rectangle_outline((((SCREEN_WIDTH/2) + r_w*2) - r_w/2)+5, r_h*value, r_w, r_h, white)
 
 def draw_pic():
     r_w = 100
@@ -77,8 +14,6 @@ def draw_pic():
     second_floor = 1.97
     first_floor = 1
     
-    
-
     # 2
     draw_two(r_w, r_h, fourth_floor, True, (arcade.color.SKY_BLUE), (arcade.color.SKY_BLUE))
     draw_four(r_w, r_h, third_floor, True, (arcade.color.SKY_BLUE), (arcade.color.SKY_BLUE), (arcade.color.SKY_BLUE), (arcade.color.YELLOW))
